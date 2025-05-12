f9afe09611e50dbd8a11dcbdf9d8e69af523e55b
Jojo Lindell <johannes152@hotmail.com>
2025-05-12 12:32:48 +0200

    Quick Update

---
+++ Controllers/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YappingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class Category(Services.MongoDB mongoDB) : ControllerBase
+    {
+        private readonly Services.MongoDB _dB = mongoDB;
+
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetCategory(string id)
+        {
+            var cat = await _dB.GetCategory(id);
+
+            if (cat != null)
+                return Ok(cat);
+            else return BadRequest("No category found");
+        }
+    }
+}
---
+++ Controllers/Comment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YappingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/post")]
+    public class Comment(Services.MongoDB mongoDb) : ControllerBase
+    {
+        private readonly Services.MongoDB _db = mongoDb;
+
+        [HttpGet("latest")]
+        public async Task<IActionResult> LatestPost(DateTime lastCreatedAt)
+        {
+            var post = await _db.GetLatestPosts(lastCreatedAt);
+            if (post != null)
+                return Ok(post);
+            else return BadRequest("No posts found");
+        }
+    }
+}
